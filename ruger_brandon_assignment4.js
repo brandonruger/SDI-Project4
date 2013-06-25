c6c40f38ccd16404c4a7d571ae79cc50cb8dadff
r02548 <bruger@fullsail.edu>
2013-06-24 22:36:12 -0400

    Set up function to convert strings to numbers.

--- ruger_brandon_assignment4.js
+++ ruger_brandon_assignment4.js
@@ -2,7 +2,7 @@
 
 // Number Library
 
-// Format a number to use a specific number of decimal places
+// Format a number to use a specific number of decimal places:
 
 var numberToConvert;
 var numberOfDecPlaces;
@@ -13,7 +13,19 @@ getFormattedNumber = function(numberToConvert, numberOfDecPlaces){
     
 };
 
+// end format number function
 
+// Given a string number, turn it into an actual number:
+
+var stringNumber;
+
+convertStringtoNumber = function(stringNumber){
+    if (isNaN(stringNumber)) {
+        console.log(stringNumber + " is not a number!");
+    } else {
+        var stringToInteger = parseInt(stringNumber);
+    } return stringToInteger;
+};
 
 
 
@@ -21,6 +33,11 @@ getFormattedNumber = function(numberToConvert, numberOfDecPlaces){
 
 var convertNumber = getFormattedNumber(5.14234, 2);
 console.log("When 5.14234 is formatted to 2 decimal places, the number is: " + convertNumber);
+
+var stringToNumberConversion = convertStringtoNumber("52");
+console.log("When the string \"52\" is converted to a number, the result is: " + stringToNumberConversion);
+stringToNumberConversion = convertStringtoNumber("Hello");
+console.log("When trying to convert the string \"Hello\" to a number, the result is: " + stringToNumberConversion);
 
 
 
