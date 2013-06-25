43de4db8fd9e6eff8aa3accd524044673a48a1d8
r02548 <bruger@fullsail.edu>
2013-06-24 23:06:45 -0400

    Set up function to check if a string is a URL.

--- ruger_brandon_assignment4.js
+++ ruger_brandon_assignment4.js
@@ -26,6 +26,24 @@ convertStringtoNumber = function(stringNumber){
         var stringToInteger = parseInt(stringNumber);
     } return stringToInteger;
 };
+// end convert string to number function
+
+// String Library
+
+// Is the string a URL (Does it start with http:// or https://)?
+
+var isStringAUrl = function(potentialURL){
+    if (potentialURL.indexOf("http://") == 0) {
+        return true;
+    } else if (potentialURL.indexOf("https://") == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// end is the string a URL
+
 
 
 
@@ -38,6 +56,13 @@ var stringToNumberConversion = convertStringtoNumber("52");
 console.log("When the string \"52\" is converted to a number, the result is: " + stringToNumberConversion);
 stringToNumberConversion = convertStringtoNumber("Hello");
 console.log("When trying to convert the string \"Hello\" to a number, the result is: " + stringToNumberConversion);
+
+var checkIfStringIsURL = isStringAUrl("http://www.facebook.com");
+console.log("It is " + checkIfStringIsURL + " that this is a URL.");
+checkIfStringIsURL = isStringAUrl("https://www.google.com");
+console.log("It is " + checkIfStringIsURL + " that this is a URL.");
+checkIfStringIsURL = isStringAUrl("ww.ebay.com");
+console.log("It is " + checkIfStringIsURL + " that this is a URL.");
 
 
 
