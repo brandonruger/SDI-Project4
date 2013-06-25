c33c95a20765aa9a16906fab50e4d0be66aa129a
r02548 <bruger@fullsail.edu>
2013-06-24 20:27:46 -0400

    Set up formatting a number to use a specific number of decimal places.

--- ruger_brandon_assignment4.js
+++ ruger_brandon_assignment4.js
@@ -1,1 +1,13 @@
 //alert("JavaScript works!");
+
+// Number Library
+
+// Format a number to use a specific number of decimal places
+
+var numberToConvert = 5.1492
+
+console.log(numberToConvert + " when fixed to 2 decimal places = " + numberToConvert.toFixed(2));
+// end format number
+
+
+
