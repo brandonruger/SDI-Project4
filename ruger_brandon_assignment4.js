3042e109379173eff0817e7b203c3e6e9e2d6a07
r02548 <bruger@fullsail.edu>
2013-06-25 22:26:42 -0400

    Set all functions so they are within my master library function.

--- ruger_brandon_assignment4.js
+++ ruger_brandon_assignment4.js
@@ -1,68 +1,66 @@
 //alert("JavaScript works!");
 
-// Number Library
+// Brandon Ruger
+// SDI 1306
+// Project 4 - Creation of Library
+
+
+var myLibrary = function(){
 
 // Format a number to use a specific number of decimal places:
 
-var numberToConvert;
-var numberOfDecPlaces;
-
-getFormattedNumber = function(numberToConvert, numberOfDecPlaces){
-    var convertedNumber = numberToConvert.toFixed(numberOfDecPlaces);
-    return convertedNumber;
+    var getFormattedNumber = function(numberToConvert, numberOfDecPlaces){
+        var convertedNumber = numberToConvert.toFixed(numberOfDecPlaces);
+        return convertedNumber;
     
-};
+    }; // end format number function
 
-// end format number function
 
 // Given a string number, turn it into an actual number:
 
-var stringNumber;
 
-convertStringtoNumber = function(stringNumber){
-    if (isNaN(stringNumber)) {
-        console.log(stringNumber + " is not a number!");
-    } else {
-        var stringToInteger = parseInt(stringNumber);
-    } return stringToInteger;
-};
-// end convert string to number function
+    var convertStringToNumber = function(stringNumber){
+        if (isNaN(stringNumber)) {
+            console.log(stringNumber + " is not a number!");
+        } else {
+            var stringToInteger = parseInt(stringNumber);
+        } return stringToInteger;
+    }; // end convert string to number function
 
-// String Library
 
 // Is the string a URL (Does it start with http:// or https://)?
 
-var isStringAUrl = function(potentialURL){
-    if (potentialURL.indexOf("http://") == 0) {
-        return true;
-    } else if (potentialURL.indexOf("https://") == 0) {
-        return true;
-    } else {
-        return false;
+    var isStringAUrl = function(potentialUrl){
+        if (potentialUrl.indexOf("http://") == 0) {
+            return true;
+        } else if (potentialUrl.indexOf("https://") == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }; // end is the string a URL
+    
+    return {
+        "getFormattedNumber": getFormattedNumber,
+        "convertStringToNumber": convertStringToNumber,
+        "isStringAUrl": isStringAUrl
     }
+
 };
-
-// end is the string a URL
-
-
 
 
 // Main Code
 
-var convertNumber = getFormattedNumber(5.14234, 2);
-console.log("When 5.14234 is formatted to 2 decimal places, the number is: " + convertNumber);
+var brandonsLibrary = new myLibrary();
 
-var stringToNumberConversion = convertStringtoNumber("52");
-console.log("When the string \"52\" is converted to a number, the result is: " + stringToNumberConversion);
-stringToNumberConversion = convertStringtoNumber("Hello");
-console.log("When trying to convert the string \"Hello\" to a number, the result is: " + stringToNumberConversion);
+console.log("When 5.14234 is formatted to 2 decimal places, the number is: " + brandonsLibrary.getFormattedNumber(5.14234, 2));
 
-var checkIfStringIsURL = isStringAUrl("http://www.facebook.com");
-console.log("It is " + checkIfStringIsURL + " that this is a URL.");
-checkIfStringIsURL = isStringAUrl("https://www.google.com");
-console.log("It is " + checkIfStringIsURL + " that this is a URL.");
-checkIfStringIsURL = isStringAUrl("ww.ebay.com");
-console.log("It is " + checkIfStringIsURL + " that this is a URL.");
+console.log("When the string \"52\" is converted to a number, the result is: " + brandonsLibrary.convertStringToNumber("52"));
+console.log("When trying to convert the string \"Hello\" to a number, the result is: " + brandonsLibrary.convertStringToNumber("Hello"));
+
+console.log("It is " + brandonsLibrary.isStringAUrl("http://www.facebook.com") + " that this is a URL.");
+console.log("It is " + brandonsLibrary.isStringAUrl("https://www.google.com") + " that this is a URL.");
+console.log("It is " + brandonsLibrary.isStringAUrl("ww.ebay.com") + " that this is a URL.");
 
 
 
